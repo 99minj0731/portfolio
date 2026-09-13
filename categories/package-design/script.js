c3903ce08eb7e88abb253bbbf1d1c7b4f47d8c1a
guo95cp <guo95cp@gmail.com>
2026-09-13 19:28:49 +0900

    카테고리 프로젝트 별 분류

--- categories/package-design/script.js
+++ categories/package-design/script.js
@@ -1,1 +1,11 @@
-// Package Design 페이지 전용 동작을 아래에 추가하세요.
+const tabs = document.querySelectorAll(".project-tab");
+const frame = document.querySelector("#project-frame");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    tabs.forEach((item) => item.classList.remove("is-active"));
+    tab.classList.add("is-active");
+
+    frame.src = tab.dataset.src;
+  });
+});
